f9810d75176c49bcfbba6e81934f37e425ea8c71
Christian <christian.obetti@akera.it>
2024-12-28 17:43:37 +0100

    Moved portfolio layout side to its own component

--- app/routes/portfolio/index.tsx
+++ app/routes/portfolio/index.tsx
@@ -1,3 +1,9 @@
+import { redirect } from 'react-router'
+
+export async function loader() {
+  return redirect('./home');
+} 
+
 export default function PortfolioIndex() {
   return <div>
     TEST
--- app/routes/portfolio/layout.tsx
+++ app/routes/portfolio/layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router';
+import { Link, Outlet } from 'react-router';
 import { GithubLogo, LinkedinLogo, Navbar } from '../../components';
 import type { NavbarMenuItem } from '../../models/navbar';
 
@@ -13,13 +13,7 @@ export default function PortfolioLayout() {
 
   return (
     <div className="h-screen flex py-5">
-      <aside className="flex flex-col items-center gap-2">
-        <div className="h-40 border-l-2 border-gray-600"></div>
-        <div className="flex flex-col gap-2 px-3">
-          <LinkedinLogo />
-          <GithubLogo />
-        </div>
-      </aside>
+      <PortfolioLayoutSide />
       <div className="flex flex-col w-full px-20">
         <Navbar title="Christian" menuItems={menuItems} />
         <main>
@@ -27,5 +21,21 @@ export default function PortfolioLayout() {
         </main>
       </div>
     </div>
+  );
+}
+
+function PortfolioLayoutSide() {
+  return (
+    <aside className="flex flex-col items-center gap-2">
+      <div className="h-40 border-l-2 border-gray-600"></div>
+      <div className="flex flex-col gap-2 px-3">
+        <Link to="https://www.linkedin.com/" target="_blank" title="Linkedin">
+          <LinkedinLogo />
+        </Link>
+        <Link to="https://www.github.com/" target="_blank" title="GitHub">
+        <GithubLogo />
+        </Link>
+      </div>
+    </aside>
   );
 }
